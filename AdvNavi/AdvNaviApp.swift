b23b7d30c6a218c7829d6470ecd7892c4509dd90
Mikkel Hauge <mikkelhauge84+github@gmail.com>
2026-06-11 23:16:14 +0200

    initial commit

---
+++ AdvNavi/AdvNaviApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvNaviApp.swift
+//  AdvNavi
+//
+//  Created by Mikkel Hauge on 11/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct AdvNaviApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
